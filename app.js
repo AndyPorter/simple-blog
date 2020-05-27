5330937e69276e07fa0119432943f0267c820e1a
Andrew Porter <andyporter@Andrews-MacBook-Air.local>
2020-05-27 18:14:37 +0100

    add mongoose to handle mongo db connections

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var app = express();
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+// database connection
+var mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/node-blog")
+
 // routes 
 app.get("/", (req, res) => {
    res.render('index');
