925147bca52f7900d19b7e20c45bbfcc12e0323e
Andrew Porter <andyporter@Andrews-MacBook-Air.local>
2020-05-27 17:27:26 +0100

    add index page and route

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 var express = require('express');
 var app = express();
 
+// EJS as rendering engine
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// routes 
+app.get("/", (req, res) => {
+   res.render('index');
+});
+
+
 // Listen
 app.listen(3030, () => {
     console.log('Server listing on 3030');
