cc969b1a96af343b44a950c1c62f980a96fcde48
Andrew Porter <andyporter@Andrews-MacBook-Air.local>
2020-05-28 17:57:25 +0100

    add: add blog post route and save function

--- app.js
+++ app.js
@@ -15,12 +15,32 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
+// db schema and model
+var postSchema = new mongoose.Schema({ body: String });
+var Post = mongoose.model('Post', postSchema);
 
+// ****************
 // routes 
+// ****************
+
+// index page get
 app.get("/", (req, res) => {
    res.render('index');
 });
 
+// add blog post
+app.post('/addpost', (req, res) => {
+    var postData = new Post(req.body);
+    postData.save().then( result => {
+        res.redirect('/');
+    }).catch(err => {
+        res.status(400).send("Unable to save data");
+    });
+});
+
+// ****************
+// end routes 
+// ****************
 
 // Listen
 app.listen(3030, () => {
